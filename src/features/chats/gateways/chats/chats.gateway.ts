ab7e20eacce4a0930adbb29018a79b48e4d2b4d2
nzhukovskiy <zhukovskiy.sfedu@gmail.com>
2025-02-16 20:18:44 +0300

    Chat gateway fixes

--- src/features/chats/gateways/chats/chats.gateway.ts
+++ src/features/chats/gateways/chats/chats.gateway.ts
@@ -1,9 +1,28 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer
+} from "@nestjs/websockets";
+import { Server } from 'socket.io';
 
-@WebSocketGateway(3001, { cors: { origin: '*' } })
-export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+@WebSocketGateway(3001)
+export class ChatsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+    @WebSocketServer()
+    server: Server;
+
+    @SubscribeMessage('message')
+    handleMessage(client: any, payload: any) {
+        console.log('Received message:', payload);
+        client.emit('response', 'Message received!');
+    }
+
+    handleConnection(client: any, ...args: any[]): any {
+        console.log('Client connected:', client.id);
+    }
+
+    handleDisconnect(client: any): any {
+        console.log('Client disconnected:', client.id);
+    }
 }

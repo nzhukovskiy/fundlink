82316921c99d1cd91e252554c6f1f940a7d09a05
Nikita Zhukovskiy <zhukovskiy.sfedu@gmail.com>
2025-05-25 21:33:52 +0300

    Added sqlalchemy for connections

--- probability_recommendation_service.py
+++ probability_recommendation_service.py
@@ -3,124 +3,144 @@ from math import exp
 import os
 from flask import jsonify
 import psycopg2
+from sqlalchemy import create_engine, text
+
+DATABASE_URL = os.getenv("POSTGRES_URL")
+engine = create_engine(DATABASE_URL)
 
 ALPHA=50000000
 
+# db_pool = pool.SimpleConnectionPool(
+#     minconn=1,
+#     maxconn=50,
+#     dsn=os.getenv("POSTGRES_URL")
+# )
+
 class ProbabilityRecommendationService():
     def calculate_probs_recommendations(self, id):
-        connection = psycopg2.connect(os.getenv("POSTGRES_URL"))
-        cursor = connection.cursor()
-        cursor.execute("""
-            SELECT 
-                i.id AS investor_id,
-                i."name" ,
-                i.surname ,
-                s.id AS startup_id,
-                s.title,
-                s.description,
-                iv.amount ,
-                iv."date" 
-            from investor i
-                JOIN investment iv ON i.id = iv."investorId"
-                JOIN funding_round fr ON iv."fundingRoundId"  = fr.id
-                JOIN startup s ON fr."startupId"  = s.id
-                       where iv.stage = 'COMPLETED'
-        """)
-        rows = cursor.fetchall()
-        investor_data = {}
-        startup_data = {}
-        investor_weights = {}
-        startups_weights = {}
-        for row in rows:
-            investor_id, name, surname, startup_id, startup_title, startup_description, investment_amount, investment_date = row
-            investment_amount = int(investment_amount)
+        with engine.connect() as connection:
+            cursor = connection.execute(text("""
+                SELECT 
+                    i.id AS investor_id,
+                    i."name" ,
+                    i.surname ,
+                    s.id AS startup_id,
+                    s.title,
+                    s.description,
+                    iv.amount ,
+                    iv."date" 
+                from investor i
+                    JOIN investment iv ON i.id = iv."investorId"
+                    JOIN funding_round fr ON iv."fundingRoundId"  = fr.id
+                    JOIN startup s ON fr."startupId"  = s.id
+                        where iv.stage = 'COMPLETED'
+            """))
+            rows = cursor.fetchall()
+            investor_data = {}
+            startup_data = {}
+            investor_weights = {}
+            startups_weights = {}
+            for row in rows:
+                investor_id, name, surname, startup_id, startup_title, startup_description, investment_amount, investment_date = row
+                investment_amount = int(investment_amount)
 
-            self.__construct_dict(investor_id, investor_data, startup_id, 
-                {
-                    "name": name,
-                    "surname": surname,
-                    "startups": {}
-                },
-                {
-                    "title": startup_title,
-                    "description": startup_description,
-                    "investments": []
-                },
-                "startups", investment_amount, investment_date)
-            self.__construct_dict(startup_id, startup_data, investor_id, 
-                {
-                    "title": startup_title,
-                    "description": startup_description,
-                    "investors": {}
-                },
-                {
-                    "name": name,
-                    "surname": surname,
-                    "investments": []
-                },
-                "investors", investment_amount, investment_date)
-            startups_weights[startup_id] = 0
-            investor_weights[investor_id] = 1 if investor_id == id else 0
+                self.__construct_dict(investor_id, investor_data, startup_id, 
+                    {
+                        "name": name,
+                        "surname": surname,
+                        "startups": {}
+                    },
+                    {
+                        "title": startup_title,
+                        "description": startup_description,
+                        "investments": []
+                    },
+                    "startups", investment_amount, investment_date)
+                self.__construct_dict(startup_id, startup_data, investor_id, 
+                    {
+                        "title": startup_title,
+                        "description": startup_description,
+                        "investors": {}
+                    },
+                    {
+                        "name": name,
+                        "surname": surname,
+                        "investments": []
+                    },
+                    "investors", investment_amount, investment_date)
+                startups_weights[startup_id] = 0
+                investor_weights[investor_id] = 1 if investor_id == id else 0
 
-        for investor_id in investor_data.keys():
-            total_investments = self.__calculate_total_investments(investor_data[investor_id])
-            for startup_id in investor_data[investor_id]["startups"].keys():
-                total_startup_investment = self.__calculate_total_investment_received(startup_data[startup_id])
-                connection_sum = 0
-                for investment in investor_data[investor_id]["startups"][startup_id]["investments"]:
-                    time_decay = 1
-                    connection_sum += time_decay*investment["investment_amount"]
-                investor_data[investor_id]["startups"][startup_id]["weight"] = connection_sum/total_investments
-                startup_data[startup_id]["investors"][investor_id]["weight"] = connection_sum/total_startup_investment
+            for investor_id in investor_data.keys():
+                total_investments = self.__calculate_total_investments(investor_data[investor_id])
+                for startup_id in investor_data[investor_id]["startups"].keys():
+                    total_startup_investment = self.__calculate_total_investment_received(startup_data[startup_id])
+                    connection_sum = 0
+                    for investment in investor_data[investor_id]["startups"][startup_id]["investments"]:
+                        time_decay = 1
+                        connection_sum += time_decay*investment["investment_amount"]
+                    investor_data[investor_id]["startups"][startup_id]["weight"] = connection_sum/total_investments
+                    startup_data[startup_id]["investors"][investor_id]["weight"] = connection_sum/total_startup_investment
 
 
-        for i in range(2):
-            self.__calculate_part_weights(investor_weights, startups_weights, investor_data, "startups")
-            self.__calculate_part_weights(startups_weights, investor_weights, startup_data, "investors", last_calculation=True if i == 1 else False)
+            for i in range(2):
+                self.__calculate_part_weights(investor_weights, startups_weights, investor_data, "startups")
+                self.__calculate_part_weights(startups_weights, investor_weights, startup_data, "investors", last_calculation=True if i == 1 else False)
 
-        startup_data_top = [
-                {"id": k, **v}
-                for k, v in sorted(
-                    ((k, v) for k, v in startup_data.items() if k not in investor_data[id]["startups"]),
-                    key=lambda x: startups_weights[x[0]],
-                    reverse=True
-                )[:20]
-            ]
-        
-        startup_ids = [startup["id"] for startup in startup_data_top]
-        placeholders = ', '.join(['%s'] * len(startup_ids))
-        query = f'''
-            SELECT * FROM funding_round
-            WHERE "startupId" IN ({placeholders}) AND "isCurrent" = true
-        '''
-        cursor.execute(query, startup_ids)
-        funding_rounds = cursor.fetchall()
+            startup_data_top = [
+                    {"id": k, **v}
+                    for k, v in sorted(
+                        ((k, v) for k, v in startup_data.items() if k not in investor_data[id]["startups"]),
+                        key=lambda x: startups_weights[x[0]],
+                        reverse=True
+                    )[:20]
+                ]
+            
+            startup_ids = [startup["id"] for startup in startup_data_top]
+            # placeholders = ', '.join(['%s'] * len(startup_ids))
+            # query = f'''
+            #     SELECT * FROM funding_round
+            #     WHERE "startupId" IN ({placeholders}) AND "isCurrent" = true
+            # '''
+            # cursor.execute(query, startup_ids)
+            # funding_rounds = cursor.fetchall()
 
-        funding_round_map = {}
-        for row in funding_rounds:
-            (id, stage, funding_goal, current_raised, start_date, end_date, is_current,
-            startup_id, notifications_sent, pre_money) = row
-            funding_round_map[startup_id] = {
-                "id": id,
-                "startup_id": startup_id,
-                "stage": stage,
-                "fundingGoal": funding_goal,
-                "currentRaised": current_raised,
-                "start_date": start_date,
-                "end_date": end_date,
-                "isCurrent": is_current
-            }
 
-        for startup in startup_data_top:
-            funding_round = funding_round_map.get(startup["id"])
-            startup["fundingRounds"] = [funding_round] if funding_round else []
+            query = text("""
+                SELECT * FROM funding_round
+                WHERE "startupId" IN :startup_ids AND "isCurrent" = true
+            """).bindparams(startup_ids=tuple(startup_ids))
+            funding_rounds = connection.execute(query)
 
-        cursor.close()
-        connection.close()
-        return jsonify({
-            "startup_weights": startups_weights,
-            "recommendedStartups": startup_data_top
-        })
+            funding_round_map = {}
+            for row in funding_rounds:
+                (id, stage, funding_goal, current_raised, start_date, end_date, is_current,
+                startup_id, notifications_sent, pre_money) = row
+                funding_round_map[startup_id] = {
+                    "id": id,
+                    "startup_id": startup_id,
+                    "stage": stage,
+                    "fundingGoal": funding_goal,
+                    "currentRaised": current_raised,
+                    "start_date": start_date,
+                    "end_date": end_date,
+                    "isCurrent": is_current
+                }
+
+            for startup in startup_data_top:
+                funding_round = funding_round_map.get(startup["id"])
+                startup["fundingRounds"] = [funding_round] if funding_round else []
+
+            # cursor.close()
+            return jsonify({
+                "startup_weights": startups_weights,
+                "recommendedStartups": startup_data_top
+            })
+        # finally:
+        #     if cursor is not None:
+        #         cursor.close()
+        #     db_pool.putconn(connection)
+
 
     def __construct_dict(self, parent_id, collection, child_id, parent_dict, child_dict, child_key, investment_amount, investment_date):
         if parent_id not in collection:

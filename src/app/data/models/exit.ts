639a5146eef6ab81da3665ba9eea7f0adce27634
nzhukovskiy <zhukovskiy.sfedu@gmail.com>
2025-05-30 16:53:06 +0300

    Added exit fields

--- src/app/data/models/exit.ts
+++ src/app/data/models/exit.ts
@@ -12,11 +12,20 @@ export class Exit {
 
     startup: Startup;
 
-    constructor(id: number, date: Date, type: ExitType, value: string, startup: Startup) {
+    sharePrice: string;
+
+    totalShares: string;
+
+    lockupPeriodDays: number;
+
+    constructor(id: number, date: Date, type: ExitType, value: string, startup: Startup, sharePrice: string, totalShares: string, lockupPeriodDays: number) {
         this.id = id;
         this.date = date;
         this.type = type;
         this.value = value;
         this.startup = startup;
+        this.sharePrice = sharePrice;
+        this.totalShares = totalShares;
+        this.lockupPeriodDays = lockupPeriodDays;
     }
 }
